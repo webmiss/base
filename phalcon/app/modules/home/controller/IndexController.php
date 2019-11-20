cf12ff986729a575240b1dfc07d2f9da819d9311
webmis <klingsoul@163.com>
2019-11-20 17:06:44 +0800

    Web API

--- phalcon/app/modules/home/controller/IndexController.php
+++ phalcon/app/modules/home/controller/IndexController.php
@@ -2,13 +2,15 @@
 
 namespace app\modules\home\controller;
 
+use app\controller\Base;
+
 /**
 * 网站：首页
 */
 class IndexController extends Base{
 
 	public function indexAction(){
-    return md5('sharefoot.db.webmis');
+    return self::getJSON(['code'=>0]);
     // $this->view->setTemplateAfter('main');
 		// return $this->view->pick('index/index');
 	}
