fa21820fb01be2a0b964dc54e90b445e3db1bc9b
webmis <klingsoul@163.com>
2019-11-26 19:02:08 +0800

    通知栏消息权限

--- app/lib/library/Persmission.dart
+++ app/lib/library/Persmission.dart
@@ -2,16 +2,17 @@ import 'package:permission_handler/permission_handler.dart';
 
 /* 检测并获取权限 */
 Future<bool> persmission(String type) async{
+  PermissionGroup group;
   // 用户定位
   if(type=='locationWhenInUse'){
-    PermissionStatus location = await PermissionHandler().checkPermissionStatus(PermissionGroup.locationWhenInUse);
-    if(location == PermissionStatus.denied){
-      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.locationWhenInUse]);
-      print(permissions);
-      return false;
-    }else{
-      return true;
-    }
+    group = PermissionGroup.locationWhenInUse;
   }
-  return false;
+  // 开启
+  PermissionStatus status = await PermissionHandler().checkPermissionStatus(group);
+  if(status == PermissionStatus.denied){
+    await PermissionHandler().requestPermissions([group]);
+    return false;
+  }else{
+    return true;
+  }
 }
