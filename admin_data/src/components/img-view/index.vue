3aabb1981c16e5407994142b82fe8192430d542e
webmis <klingsoul@163.com>
2020-05-27 10:57:47 +0800

    清理

--- admin_data/src/components/img-view/index.vue
+++ admin_data/src/components/img-view/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-if="show" class="imgview_bg" :style="{backgroundColor: 'rgba(0,0,0,'+opacity+')'}">
+  <div v-if="show" ref="ImgBG" class="imgview_bg" :style="{backgroundColor: 'rgba(0,0,0,'+opacity+')'}">
     <div class="imgview_close" @click="close()"><i class="el-icon-close"></i></div>
     <img class="verticalCenter" :src="img" />
   </div>
 </template>
 
 <style scoped>
-.imgview_bg{position: fixed; z-index: 1000; width: 100%; height: 100%; top: 0; left: 0;}
+.imgview_bg{position: fixed; z-index: 1000; width: 100%; height: 100%; top: 0; left: 0; opacity: 0; transition: all .3s ease;}
 .imgview_close{position: absolute; z-index: 11; border-radius: 50%; cursor: pointer; font-size: 28px; color: #FFF; text-align: center; width: 48px; height: 48px; line-height: 48px; top: 10px; right: 10px;}
 .imgview_close:hover{color: #FF6600;}
 .imgview_left{}
@@ -35,30 +35,48 @@ export default {
   },
   methods:{
 
-    /* 打开图片 */
+    /* 打开 */
     open(imgs,index){
+      // 数据
       this.index = index || 0;
       this.imgs = imgs || [];
       this.img = this.imgs[this.index];
+      // 宽高
+      this.scale();
+      // 动画
+      setTimeout(()=>{
+        let bg = this.$refs.ImgBG;
+        bg.style.opacity = 1;
+      },300);
     },
+
+    /* 缩放 */
+    scale(){
+      let img = new Image();
+      img.src = this.img;
+      img.onload = function (){
+        let w = document.body.clientWidth;
+        let h = document.body.clientHeight;
+        let src_size = this.width/this.height;
+        let dst_size = w/h;
+        // 是否缩放
+        if(this.width>w || this.height>h){
+          
+          console.log(src_size,dst_size);
+        }
+      }
+    },
+
 
     /* 关闭 */
     close(){
-      this.$emit('show',false);
+      setTimeout(()=>{
+        this.$emit('show',false);
+      },300);
+      // 动画
+      let bg = this.$refs.ImgBG;
+      bg.style.opacity = 0;
     },
-
-    /* 动作菜单 */
-    getAction(url){
-      Inc.post('Usermain/getMenusAction',{token:Inc.storage.getItem('token'),url:url},(res)=>{
-        const d = res.data;
-        if(d.code==0) this.actions = d.menuAction;
-      });
-    },
-
-    /* 触发事件 */
-    openAction(action){
-      this.$emit('action',action);
-    }
 
   }
 }
