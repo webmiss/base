0771c25b637420c6cd0621192a657047a7b3efe3
webmis <klingsoul@163.com>
2020-06-19 10:17:41 +0800

    图片预览

--- admin/src/components/img-view/index.vue
+++ admin/src/components/img-view/index.vue
@@ -13,12 +13,12 @@
       <i class="el-icon-arrow-right" v-if="(index+1)!=imgs.length"></i>
     </div>
     <!-- 信息 -->
-    <div class="imgview_info flex_center">
-      <div class="nowrap">
+    <div class="imgview_info">
+      <span class="nowrap">
         <span>名称: {{info.name}}</span>
         <span>大小: {{info.size}}</span>
         <span>页码: {{index+1}}/{{imgs.length}}</span>
-      </div>
+      </span>
     </div>
     <!-- 关闭 -->
     <div class="imgview_close" @click="close()"><i class="el-icon-close"></i></div>
@@ -26,26 +26,6 @@
     <div class="imgview_full" @click="Fullscreen()"><i class="el-icon-rank"></i></div>
   </div>
 </template>
-
-<style scoped>
-.imgview_bg{position: fixed; z-index: 1000; width: 100%; height: 100%; top: 0; left: 0; opacity: 0; transition: all .3s ease;}
-.imgview_load,.imgview_close,.imgview_full,.imgview_info,.imgview_left,.imgview_right{position: absolute; z-index: 100;}
-.imgview_load,.imgview_close,.imgview_full{cursor: pointer; border-radius: 50%; font-size: 28px; color: #FFF; text-align: center; width: 48px; height: 48px; line-height: 48px; }
-.imgview_img{transition: all .3s ease;}
-.imgview_load{left: 0; right: 0; top: 0; bottom: 0; margin: auto;}
-.imgview_close{top: 10px; right: 10px;}
-.imgview_close:hover{color: #FF6600;}
-.imgview_full{bottom: 10px; right: 10px;}
-.imgview_info{height: 40px; line-height: 40px; left: 0; right: 0; bottom: 20px;}
-.imgview_info div{border-radius: 20px; padding: 0 20px; color: #EEE; background-color: rgba(0,0,0,0.3);}
-.imgview_info span{padding: 0 5px;}
-.imgview_left,.imgview_right{top: 0; width: 50%; height: 100%;}
-.imgview_left i,.imgview_right i{position: absolute; color: #FFF; font-size: 28px; top: 50%; transform: translate3d(0,-50%,0);}
-.imgview_left{left: 0;}
-.imgview_left i{left: 10px;}
-.imgview_right{right: 0;}
-.imgview_right i{right: 10px;}
-</style>
 
 <script>
 export default {
