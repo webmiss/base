825fae8e5c4ce8082d56860aaec5f2551331630a
webmis <klingsoul@163.com>
2020-06-23 17:03:20 +0800

    socket

--- vueapp/src/library/Socket.js
+++ vueapp/src/library/Socket.js
@@ -4,6 +4,45 @@ import Plus from '@/library/Plus'
 /* Socket */
 export default {
 
+  /* 消息路由 */
+  msgRouter(d){
+    if(d.type=='group') this.msgGroup(d); //消息组
+    else if(d.type=='msg') this.msg(d); //消息
+    else if(d.type=='notify') this.msgNotify(d); //通知
+  },
+
+  /* 消息组 */
+  msgGroup(d){
+    Inc.self.$store.state.uMsg.group = d.data;
+    // 消息数
+    const data = Inc.self.$store.state.uMsg.group;
+    let num = 0;
+    for(let i in data) num += data[i].num;
+    Inc.self.$store.state.uMsg.num = num;
+  },
+
+  /* 消息 */
+  msg(d){
+    // 阅读
+    const voice = Inc.storage.getItem('voice');
+    Plus.notify(d.data.title,d.data.content,voice?true:false);
+    // 追加
+    const fid = d.data.uid==Inc.self.$store.state.uInfo.uid?d.data.fid:d.data.uid;
+    if(Inc.self.$store.state.uMsg.group[''+fid]){
+      Inc.self.$store.state.uMsg.group[''+fid].msg.push(d.data);
+      // 记录数量
+      Inc.self.$store.state.uMsg.group[''+fid].num++;
+      Inc.self.$store.state.uMsg.num++;
+    }else{
+      Inc.self.$store.state.socket.send(JSON.stringify({type:'group'}));
+    }
+  },
+
+  /* 通知 */
+  msgNotify(d){
+    console.log(d);
+  },
+
   /* 启动 */
   start(){
     // 重启Socket
@@ -46,48 +85,11 @@ export default {
     /* 接收 */
     Inc.self.$store.state.socket.onmessage = (res)=>{
       const d = JSON.parse(res.data);
-      console.log(d);
       // 是否成功
       if(d.code!=0) return Inc.toast(d.msg);
-      /* 消息组 */
-      if(d.code==0 && d.type=='group'){
-        Inc.self.$store.state.uMsg.group = d.data;
-        // 消息数
-        this._getMsgNum();
-      }
-      /* 消息 */
-      else if(d.code==0 && d.type=='msg'){
-        // 阅读
-        const voice = Inc.storage.getItem('voice');
-        // console.log(voice);
-        // Plus.notify(d.data.title,d.data.content,(obj)=>{
-        //   obj.close();
-        // },voice?true:false);
-        // 追加
-        const fid = d.data.uid==Inc.self.$store.state.uInfo.uid?d.data.fid:d.data.uid;
-        if(Inc.self.$store.state.uMsg.group[''+fid]){
-          Inc.self.$store.state.uMsg.group[''+fid].msg.push(d.data);
-          // 记录数量
-          Inc.self.$store.state.uMsg.group[''+fid].num++;
-          Inc.self.$store.state.uMsg.num++;
-        }else{
-          Inc.self.$store.state.socket.send(JSON.stringify({type:'group'}));
-        }
-      }
-      /* 通知 */
-      else if(d.code==0 && d.type=='notify'){
-        // console.log(d);
-        // console.log(Inc.self.$store.state.uNotify);
-      }
+      // 消息路由
+      this.msgRouter(d);
     };
-  },
-
-  /* 消息数 */
-  _getMsgNum(){
-    const data = Inc.self.$store.state.uMsg.group;
-    let num = 0;
-    for(let i in data) num += data[i].num;
-    Inc.self.$store.state.uMsg.num = num;
   },
   
   /* 关闭 */
