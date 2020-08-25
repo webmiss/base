3d76aa4318eb18da97f55579d69284c32eea64fe
webmis <klingsoul@163.com>
2020-08-25 10:35:44 +0800

    左右滑动

--- flutter/lib/library/ui/ui-navigator-to.dart
+++ flutter/lib/library/ui/ui-navigator-to.dart
@@ -5,7 +5,8 @@ class NavigatorTo{
 
   /* 跳转 */
   static void push(context,{page}){
-    Navigator.push(context, MaterialPageRoute(builder: (context){return page;}));
+    // Navigator.push(context, MaterialPageRoute(builder: (context){return page;}));
+    Navigator.push(context, CustomRouteSlide(page));
   }
   
   /* 返回 */
@@ -13,4 +14,40 @@ class NavigatorTo{
     Navigator.pop(context,parm);
   }
 
+}
+
+/* 左右滑动 */
+class CustomRouteSlide extends PageRouteBuilder{
+  final Widget widget;
+  CustomRouteSlide(this.widget) :super(
+    transitionDuration: const Duration(milliseconds:400),
+    pageBuilder:(
+      BuildContext context,
+      Animation<double> animation1,
+      Animation<double> animation2
+    ){
+      return widget;
+    },
+    transitionsBuilder:(
+      BuildContext context,
+      Animation<double> animation1,
+      Animation<double> animation2,
+      Widget child,
+    ){
+
+      return SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(1.0, 0.0),
+          end:Offset(0.0, 0.0)
+        ).animate(
+          CurvedAnimation(
+            parent: animation1,
+            curve: Curves.fastOutSlowIn
+          )
+        ),
+        child: child,
+      );
+
+    } 
+  ); 
 }
