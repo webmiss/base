4e0b121d0d6d30ccd5c2383fec779d95377566ce
webmis <klingsoul@163.com>
2020-09-22 19:30:24 +0800

    bug

--- flutter/lib/library/ui/ui-toast.dart
+++ flutter/lib/library/ui/ui-toast.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webmis/library/ui/ui.dart';
-import 'package:webmis/library/inc/time-set.dart';
 
 /* UI-提示 */
 class Toast {
@@ -8,46 +7,26 @@ class Toast {
   static String _msg;
   static BuildContext _context;
   static bool _show = false;
-  static var _t;
+  static double _top = 0.0;
+
   Toast(
     BuildContext context,
     String msg,
-    {int time: 3000}
+    { int time: 2000 }
   ){
 
-    /* 参数 */
+    // 参数
     assert(msg != null);
     _msg = msg;
     _context = context;
-
-    /* 状态 */
-    OverlayState overlayState = Overlay.of(context);
-
-    /* 内容 */
-    if(_overlayEntry==null){
-      _overlayEntry = OverlayEntry(builder: (BuildContext context)=>_getBox() );
-      // 插入
-      overlayState.insert(_overlayEntry);
-    }else{
-      // 刷新UI
-      _overlayEntry.markNeedsBuild();
-    }
-
-    /* 显示 */
-    setTimeout((){
-      _show = true;
-      _overlayEntry.markNeedsBuild();
-    },100);
-
-    /* 隐藏 */
-    if(_t!=null) _t.cancel();
-    _t = setTimeout((){
-      _show = false;
-      _overlayEntry.markNeedsBuild();
-    },time);
-
-    Future.delayed(Duration(seconds: 5)).then((value) {
-      // 移除层可以通过调用OverlayEntry的remove方法。
+    // 内容
+    _overlayEntry = OverlayEntry(builder: (BuildContext context)=>_getBox() );
+    // 显示
+    _show = true;
+    _top = MediaQuery.of(_context).size.height*1/10;
+    Overlay.of(context).insert(_overlayEntry);
+    // 清除
+    Future.delayed(Duration(milliseconds: time)).then((value) {
       _overlayEntry.remove();
     });
     
@@ -56,11 +35,11 @@ class Toast {
   /* 容器-动画 */
   Widget _getBox(){
     return AnimatedPositioned(
-      top: _show?MediaQuery.of(_context).size.height*1/10:0.0,
+      top: _top,
       duration: Duration(milliseconds: _show?400:200),
       child: AnimatedOpacity(
         opacity: _show?1.0:0.0,
-        duration: Duration(milliseconds: _show?400:200),
+        duration: Duration(milliseconds: _show?400:3000),
         child: Container(
           alignment: Alignment.center,
           width: MediaQuery.of(_context).size.width,
@@ -74,7 +53,7 @@ class Toast {
   Widget _getToast(){
     return Center(
       child: Card(
-        color: Color.fromRGBO(0,0,0,0.8),
+        color: Color.fromRGBO(0,0,0,0.5),
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
           child: Ui.text(_msg,fontSize: 14, color: '#FFFFFF'),
