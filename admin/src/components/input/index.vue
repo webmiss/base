825c4f9f13b786809c0ccaa2ea768ad8b9b0f363
webmis <klingsoul@163.com>
2020-10-17 08:43:18 +0800

    Input组件

--- admin/src/components/input/index.vue
+++ admin/src/components/input/index.vue
@@ -5,8 +5,8 @@
 </template>
 
 <style scoped>
-input{border: none; background: none; -webkit-appearance: none; outline: none; font: 400 14px Arial; caret-color: #6FB737;}
-input{width: calc(100% - 32px); box-sizing: border-box; height: 40px; line-height: 20px; padding: 10px 16px; border-radius: 4px; border: #DADCDF 1px solid; background-color: #F2F4F6;}
+input{border: none; background: none; -webkit-appearance: none; outline: none; font: 400 14px Arial; caret-color: #595;}
+input{width: 100%; box-sizing: border-box; height: 40px; line-height: 20px; padding: 10px 16px; border-radius: 4px; border: #DADCDF 1px solid; background-color: #F2F4F6;}
 input:hover{border: #6FB737 1px solid;}
 input:focus{outline: none;}
 </style>
