bae8f067c1791e24304bf11ee114efa2de8fee2f
webmis <klingsoul@163.com>
2020-10-17 10:20:53 +0800

    动作菜单

--- phalcon/app/modules/admin/controller/SysmenusController.php
+++ phalcon/app/modules/admin/controller/SysmenusController.php
@@ -5,38 +5,37 @@ namespace app\modules\admin\controller;
 use app\common\Base;
 use app\common\AdminToken;
 use app\model\SysMenu;
-use app\model\SysMenuAction;
 
-class SysMenusActionController extends Base {
+class SysMenusController extends Base {
 
-  static $tokenData;
+  static private $menus=[]; // 菜单
 
   /* 构造函数 */
   function initialize(){
     // 验证
-    self::$tokenData = AdminToken::verify();
+    AdminToken::verify();
   }
 
-  /* 获取[动作菜单] */
-  function getActionAction(){
-    $url = trim($this->request->get('url'));
-    // 是否为空
-    if(empty($url)) return self::getJSON(['code'=>4000,'msg'=>'获取动作不能为空!']);
-    // 菜单ID
-    $mid = SysMenu::findFirst(['url=:url:','bind'=>['url'=>$url],'columns'=>'id']);
-    if(!$mid) return self::getJSON(['code'=>4000,'msg'=>'获取动作不存在!']);
-    // 全部动作
-    $action = [];
-    $permAll = AdminToken::perm(self::$tokenData->uid);
-    $perm = $permAll[$mid->id];
-    $aMenus = SysMenuAction::find(['columns'=>'name,action,ico,perm']);
-    foreach($aMenus as $val){
-      // 匹配权限值
-			if(intval($perm)&intval($val->perm)){
-        $action[] = ['name'=>$val->name,'action'=>$val->action,'ico'=>$val->ico];
-      }
+  /* 获取[菜单] */
+	function getMenusAction(){
+    // 全部菜单
+    self::$menus = [];
+    $all = SysMenu::find(['columns'=>'id,fid,title,url,ico','order'=>'sort DESC,id'])->toArray();
+    foreach($all as $val){
+      self::$menus[$val['fid']][] = $val;
     }
-    return self::getJSON(['code'=>0,'action'=>$action]);
+    // 查询菜单
+    return self::getJSON(['code'=>0,'menus'=>self::_getMenu(0)]);
   }
-
+  // 递归菜单
+	static private function _getMenu($fid){
+    $data = [];
+    $M = isset(self::$menus[$fid])?self::$menus[$fid]:[];
+		foreach($M as $val){
+      $val['children'] = self::_getMenu($val['id']);
+      $data[] = $val;
+		}
+		return $data;
+  }
+  
 }
--- phalcon/app/modules/admin/controller/SysmenusactionController.php
+++ phalcon/app/modules/admin/controller/SysmenusactionController.php
@@ -21,12 +21,21 @@ class SysMenusActionController extends Base {
   function getActionAction(){
     $url = trim($this->request->get('url'));
     // 是否为空
-    if(empty($url)) return self::getJSON(['code'=>0,'msg'=>'获取动作不能为空!']);
+    if(empty($url)) return self::getJSON(['code'=>4000,'msg'=>'获取动作不能为空!']);
     // 菜单ID
-    $permAll = AdminToken::perm(self::$tokenData->uid);
-    self::bug($permAll);
+    $mid = SysMenu::findFirst(['url=:url:','bind'=>['url'=>$url],'columns'=>'id']);
+    if(!$mid) return self::getJSON(['code'=>4000,'msg'=>'获取动作不存在!']);
+    // 全部动作
     $action = [];
-    // self::bug($url);
+    $permAll = AdminToken::perm(self::$tokenData->uid);
+    $perm = $permAll[$mid->id];
+    $aMenus = SysMenuAction::find(['columns'=>'name,action,ico,perm']);
+    foreach($aMenus as $val){
+      // 匹配权限值
+			if(intval($perm)&intval($val->perm)){
+        $action[] = ['name'=>$val->name,'action'=>$val->action,'ico'=>$val->ico];
+      }
+    }
     return self::getJSON(['code'=>0,'action'=>$action]);
   }
 
