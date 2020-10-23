09ce997e93c3384132a4dc32153bbd7be376bcc5
webmis <klingsoul@163.com>
2020-10-23 10:24:54 +0800

    Css

--- admin/src/components/form/button/index.vue
+++ admin/src/components/form/button/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <button class="button" :disabled="disabled">
+  <button class="wm-button" :disabled="disabled">
     <slot></slot>
   </button>
 </template>
 
 <style scoped>
-button:focus,button:active:focus,button.active:focus,button.focus,button:active.focus,button.active.focus{outline: none; border-color: transparent; box-shadow:none;}
-button{border: none; background: none;}
-.button{line-height: 40px; padding: 0 24px; text-align: center; background-color: #595; color: #FFF; border-radius: 4px;}
-.button:hover{background-color: #6FB737;}
-.button:disabled{background-color: #999;}
+.wm-button:focus,button:active:focus,button.active:focus,button.focus,button:active.focus,button.active.focus{outline: none; border-color: transparent; box-shadow:none;}
+.wm-button{border: none; background: none;}
+.wm-button{line-height: 40px; padding: 0 24px; font-size: 14px; text-align: center; background-color: #595; color: #FFF; border-radius: 4px;}
+.wm-button:hover{background-color: #6FB737;}
+.wm-button:disabled{background-color: #999;}
 </style>
 
 <script>
