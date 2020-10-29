6972a1a9354f0b8261459df00f8772dd30561c95
webmis <klingsoul@163.com>
2020-10-29 18:05:16 +0800

    Popup 弹出层

--- admin/src/components/popover/index.vue
+++ admin/src/components/popover/index.vue
@@ -1,98 +1,141 @@
 <template>
-  <div class="wm-popover">
-    <!-- Top -->
-    <div class="wm-popover_top_body" v-if="type=='top'">
-      <div class="wm-popover_top">
-        <div class="tip" :class="'tip_'+effect">
-          <div :style="{width:width}"><slot name="body"></slot></div>
-        </div>
-        <div class="arrow" :class="'arrow_'+effect"></div>
-      </div>
-      <slot name="reference"></slot>
-    </div>
-    <!-- Top End -->
-    <!-- Bottom -->
-    <div class="wm-popover_bottom_body" v-if="type=='bottom'">
-      <slot name="reference"></slot>
-      <div class="wm-popover_bottom">
-        <div class="arrow" :class="'arrow_'+effect"></div>
-        <div class="tip" :class="'tip_'+effect">
-          <div :style="{width:width}"><slot name="body"></slot></div>
-        </div>
-      </div>
-    </div>
-    <!-- Bottom End -->
-    <!-- Left -->
-    <div class="wm-popover_left_body" v-if="type=='left'">
-      <div class="wm-popover_left">
-        <div class="arrow" :class="'arrow_'+effect"></div>
-        <div class="tip" :class="'tip_'+effect">
-          <div :style="{width:width}"><slot name="body"></slot></div>
-        </div>
-      </div>
-      <slot name="reference"></slot>
-    </div>
-    <!-- Left End -->
-    <!-- Right -->
-    <div class="wm-popover_right_body" v-if="type=='right'">
-      <slot name="reference"></slot>
-      <div class="wm-popover_right">
-        <div class="arrow" :class="'arrow_'+effect"></div>
-        <div class="tip" :class="'tip_'+effect">
-          <div :style="{width:width}"><slot name="body"></slot></div>
-        </div>
-      </div>
-    </div>
-    <!-- Right End -->
-  </div>
+<div v-show="show">
+  <div ref="PopupBG" class="wm-popup_bg" :style="{backgroundColor:'rgba(0,0,0,'+opacity+')'}" @click="clickBG()"></div>
+  <div ref="PopupBody" class="wm-popup_body" :style="{backgroundColor:bgColor}"><slot></slot></div>
+</div>
 </template>
 
 <style scoped>
-.wm-popover{display: inline-flex; position: relative;}
-.wm-popover .tip{position: absolute; padding: 8px 16px; line-height: 24px; border-radius: 4px; box-shadow: 0 2px 12px rgba(0,0,0,.1);}
-.wm-popover .tip_plain{border: #DCDFE6 1px solid; background-color: rgba(255,255,255,.9); color: #333;}
-.wm-popover .tip_dark{border: #000 1px solid; background-color: rgba(0,0,0,.8); color: #FFF;}
-/* Top */
-.wm-popover_top_body{display: inline-flex; flex-direction: column;}
-.wm-popover_top{display: none; position: absolute; width: 0; height: 0; left: 50%; transform: translate(-50%,0);}
-.wm-popover_top .tip{bottom: 15px; transform: translate(-50%,0);}
-.wm-popover_top .arrow{position: absolute; top: -16px; left: -8px; width: 0px; height: 0px; border: 8px solid; border-color: transparent;}
-.wm-popover_top .arrow_plain{border-top-color: #DCDFE6;}
-.wm-popover_top .arrow_dark{border-top-color: #000;}
-.wm-popover_top_body:hover .wm-popover_top{display: block;}
-/* bottom */
-.wm-popover_bottom_body{display: inline-flex; flex-direction: column;}
-.wm-popover_bottom{display: none; position: absolute; z-index: 99; width: 0; height: 0; bottom: 0; left: 50%; transform: translate(-50%,0);}
-.wm-popover_bottom .tip{top: 15px; transform: translate(-50%,0);}
-.wm-popover_bottom .arrow{position: absolute; top: 0; left: -8px; width: 0px; height: 0px; border: 8px solid; border-color: transparent;}
-.wm-popover_bottom .arrow_plain{border-bottom-color: #DCDFE6;}
-.wm-popover_bottom .arrow_dark{border-bottom-color: #000;}
-.wm-popover_bottom_body:hover .wm-popover_bottom{display: block;}
-/* Left */
-.wm-popover_left_body{display: inline-flex; flex-direction: row;}
-.wm-popover_left{display: none; position: absolute; width: 0; height: 0; top: 50%; transform: translate(0,-50%);}
-.wm-popover_left .tip{left: -15px; transform: translate(-100%,-50%);}
-.wm-popover_left .arrow{position: absolute; left: -16px; top: -8px; width: 0px; height: 0px; border: 8px solid; border-color: transparent;}
-.wm-popover_left .arrow_plain{border-left-color: #DCDFE6;}
-.wm-popover_left .arrow_dark{border-left-color: #000;}
-.wm-popover_left_body:hover .wm-popover_left{display: block;}
-/* Right */
-.wm-popover_right_body{display: inline-flex; flex-direction: row-reverse;}
-.wm-popover_right{display: none; position: absolute; width: 0; height: 0; top: 50%; transform: translate(0,-50%);}
-.wm-popover_right .tip{right: -15px; transform: translate(100%,-50%);}
-.wm-popover_right .arrow{position: absolute; right: -16px; top: -8px; width: 0px; height: 0px; border: 8px solid; border-color: transparent;}
-.wm-popover_right .arrow_plain{border-right-color: #DCDFE6;}
-.wm-popover_right .arrow_dark{border-right-color: #000;}
-.wm-popover_right_body:hover .wm-popover_right{display: block;}
+.wm-popup_bg,.wm-popup_body{position: fixed; z-index: 1000;}
+.wm-popup_bg{width: 100%; height: 100%; top: 0; left: 0; opacity: 0;}
+.wm-popup_body{opacity: 0;}
 </style>
 
 <script>
 export default {
-  name: 'Popover',
+  name: 'Popup',
   props: {
-    type: {type: String, default: 'top'}, //位置: top、bottom、left、right
-    width: {type: String, default: '160px'}, //内容宽度
-    effect: {type: String, default: 'plain'}, //样式: plain、dark
+    show: {type: Boolean, default: false},  //是否显示
+    position: {type: String, default: 'center'},  //位置
+    opacity: {type: Number, default: 0.7},  //背景透明度
+    bgColor: {type: String, default: ''}, //内容背景颜色
+    bgClose: {type: Boolean, default: true},  //点击背景关闭
+    time: {type: Number, default: 400},  //动画时间
   },
+  watch:{
+    show(val){
+      if(val) this.animation(val);
+    }
+  },
+  mounted(){
+    this.init();
+  },
+  methods:{
+
+    /* 初始化 */
+    init(){
+      const bg = this.$refs.PopupBG.style;
+      const body = this.$refs.PopupBody.style;
+      // 默认值
+      bg.opacity = 0;
+      if(this.position=='left'){
+        body.height = '110%';
+        body.left = 0;
+        body.top = 0;
+        body.opacity = 1;
+        body.transform = 'translate(-110%,0)';
+      }else if(this.position=='right'){
+        body.height = '100%';
+        body.right = 0;
+        body.top = 0;
+        body.opacity = 1;
+        body.transform = 'translate(110%,0)';
+      }else if(this.position=='top'){
+        body.width = '100%';
+        body.left = 0;
+        body.top = 0;
+        body.opacity = 1;
+        body.transform = 'translate(0,-110%)';
+      }else if(this.position=='bottom'){
+        body.width = '100%';
+        body.left = 0;
+        body.bottom = 0;
+        body.opacity = 1;
+        body.transform = 'translate(0,110%)';
+      }else{
+        body.left = '50%';
+        body.top = '30%';
+        body.opacity = 0;
+        body.transform = 'translate(-50%,-50%)';
+      }
+    },
+
+    /* 显示 */
+    showBody(){
+      const bg = this.$refs.PopupBG.style;
+      const body = this.$refs.PopupBody.style;
+      // 动画
+      bg.transitionDuration = this.time+'ms';
+      body.transitionDuration = this.time+'ms';
+      // 属性
+      bg.opacity = 1;
+      if(this.position=='left'){
+        body.transform = 'translate(-1px,0)';
+      }else if(this.position=='right'){
+        body.transform = 'translate(1px,0)';
+      }else if(this.position=='top'){
+        body.transform = 'translate(0,-1px)';
+      }else if(this.position=='bottom'){
+        body.transform = 'translate(0,1px)';
+      }else{
+        body.opacity = 1;
+        body.top = '50%';
+      }
+      // 更新状态
+      setTimeout(()=>{ this.$emit('update:show',true); },this.time);
+    },
+
+    /* 隐藏 */
+    hideBody(){
+      const bg = this.$refs.PopupBG.style;
+      const body = this.$refs.PopupBody.style;
+      // 动画
+      bg.transitionDuration = this.time+'ms';
+      body.transitionDuration = this.time+'ms';
+      // 属性
+      bg.opacity = 0;
+      if(this.position=='left'){
+        body.transform = 'translate(-110%,0)';
+      }else if(this.position=='right'){
+        body.transform = 'translate(110%,0)';
+      }else if(this.position=='top'){
+        body.transform = 'translate(0,-110%)';
+      }else if(this.position=='bottom'){
+        body.transform = 'translate(0,110%)';
+      }else{
+        body.opacity = 0;
+        body.top = '30%'
+      }
+      // 更新状态
+      setTimeout(()=>{ if(this.show) this.$emit('update:show',false); },this.time);
+    },
+
+    /* 控制动画 */
+    animation(show){
+      if(show) setTimeout(()=>{ this.showBody(); },300);
+      else this.hideBody();
+    },
+
+    /* 点击背景 */
+    clickBG(){
+      if(this.bgClose) this.animation(false);
+    },
+
+    /* 关闭 */
+    close(){
+      this.animation(false);
+    },
+
+  }
 }
 </script>
