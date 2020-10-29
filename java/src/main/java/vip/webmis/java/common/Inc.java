d574d599a85694dde2823d8d1ee40c68a050ad9f
webmis <klingsoul@163.com>
2020-10-29 13:03:12 +0800

    格式化时间

--- java/src/main/java/vip/webmis/java/common/Inc.java
+++ java/src/main/java/vip/webmis/java/common/Inc.java
@@ -1,5 +1,6 @@
 package vip.webmis.java.common;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.springframework.util.DigestUtils;
@@ -9,9 +10,13 @@ public class Inc {
 
   /* 格式化时间 */
   public static String date(String format){
+    String time=null;
+    return date(format,time);
+  }
+  public static String date(String format, String time){
     DateTimeFormatter df = DateTimeFormatter.ofPattern(format);
     LocalDateTime now = LocalDateTime.now();
-    return df.format(now);
+    return df.format(time!=null?Timestamp.valueOf(time).toLocalDateTime():now);
   }
 
   /* Md5加密 */
