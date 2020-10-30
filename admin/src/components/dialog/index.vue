7bb8aee78f49987da6fa9a6360522dd807c88b7e
webmis <klingsoul@163.com>
2020-10-30 13:43:25 +0800

    Dialog 对话框

--- admin/src/components/dialog/index.vue
+++ admin/src/components/dialog/index.vue
@@ -40,12 +40,12 @@ export default {
     title: {type: String, default: ''},  //标题
     width: {type: String, default: '420px'},  //宽
     height: {type: String, default: 'auto'},  //高
-    maxWidth: {type: String, default: '900px'},  //宽
-    maxHeight: {type: String, default: '90%'},  //高
+    maxWidth: {type: String, default: '900px'},  //最大宽
+    maxHeight: {type: String, default: '90%'},  //最大高
     titleAlign: {type: String, default: 'center'},  //标题对齐方式
     footerAlign: {type: String, default: 'center'},  //底部对齐方式
     bodyPadding: {type: String, default: '8px 16px'},  //内容间隙
-    bgColor: {type: String, default: '#FFF'},  //高
+    bgColor: {type: String, default: '#FFF'},  //内容背景颜色
     bgClose: {type: Boolean, default: false},  //点击背景关闭
     
   },
