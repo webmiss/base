ffb9331571395aa6b08c5aae4ee4694dca031409
webmis <klingsoul@163.com>
2020-10-30 15:34:17 +0800

    自动编号ID、Json转换

--- python/app/common/Inc.py
+++ python/app/common/Inc.py
@@ -1,5 +1,7 @@
 import time
+import datetime
 import hashlib
+import json
 
 # 公共函数
 class Inc:
@@ -11,3 +13,23 @@ class Inc:
   # Md5加密
   def md5(str) :
     return hashlib.md5(str.encode(encoding='utf-8')).hexdigest()
+
+  # 自动编号ID-18位
+  def getId() :
+    d = time.strftime('%Y%m%d%H%M%S',time.localtime())
+    t = datetime.datetime.now()
+    n = str(t.microsecond)[2:6]
+    return d+n
+
+  # Json转-转字符串
+  def json_encode(str) :
+    try :
+      return json.dumps(str)
+    except Exception as e :
+      return False
+  # Json-转数组
+  def json_decode(str) :
+    try :
+      return json.loads(str)
+    except Exception as e :
+      return False
