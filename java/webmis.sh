234d22538ec8aaf5efc0fc448f14c337d16d86ae
webmis <klingsoul@163.com>
2020-11-05 18:40:26 +0800

    Bash 脚本命令

---
+++ java/webmis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 帮助
+helpText(){
+  echo "用法: webmis <command> <args>"
+  echo "<command>"
+  echo "    run         运行"
+  echo "    build       打包"
+  echo "    start       服务器端启动"
+  echo "    restart     重启服务: 中断进程并且重新启动"
+  echo "    stop        停止服务"
+  echo "<args>"
+  echo "    -f          指定, 默认: $f"
+  echo ""
+}
+
+# 默认
+s=$1
+f="java-1.0.0.jar"
+
+# 参数
+while getopts ":f:h:" opt
+do
+  case $opt in
+    f) f="$OPTARG" ;;
+    ?)
+    helpText
+    exit 1;;
+  esac
+done
+
+# 运行
+if [ "$s" == "run" ]; then
+  java -jar $f
+# 打包
+elif [ "$s" == "build" ]; then
+  mvn package && rm "./$f" && cp "target/$f" ./
+# 启动
+elif [ "$s" == "start" ]; then
+  nohup java -jar $f >> log.txt &
+# 重启
+elif [ "$s" == "restart" ]; then
+  ps -aux | grep java | grep -v grep | awk {'print $2'} | xargs kill -2
+  nohup java -jar $f >> log.txt &
+# 停止
+elif [ "$s" == "stop" ]; then
+  ps -aux | grep java | grep -v grep | awk {'print $2'} | xargs kill -2
+else
+  helpText
+fi
+
---
+++ python/webmis.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# 帮助
+helpText(){
+  echo "用法: webmis <command> <args>"
+  echo "<command>"
+  echo "    run         运行"
+  echo "    start       服务器端启动"
+  echo "    restart     重启服务: 中断进程并且重新启动"
+  echo "    stop        停止服务"
+  echo "<args>"
+  echo "    -f          指定入口文件, 默认: $f"
+  echo ""
+}
+
+# 默认
+s=$1
+f="web.py"
+c="web.ini"
+
+# 参数
+while getopts ":f:c:h:" opt
+do
+  case $opt in
+    f) f="$OPTARG" ;;
+    c) c="$OPTARG" ;;
+    ?)
+    helpText
+    exit 1;;
+  esac
+done
+
+# 运行
+if [ "$s" == "run" ]; then
+  python $f
+# 启动
+elif [ "$s" == "start" ]; then
+  uwsgi --ini web.ini &
+# 重启
+elif [ "$s" == "restart" ]; then
+  ps -aux | grep uwsgi | grep -v grep | awk {'print $2'} | xargs kill -2
+  uwsgi --ini web.ini &
+# 停止
+elif [ "$s" == "stop" ]; then
+  ps -aux | grep uwsgi | grep -v grep | awk {'print $2'} | xargs kill -2
+else
+  helpText
+fi
+
