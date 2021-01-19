adb52294dda50460154a2f749c80fbbcc6884164
webmis <klingsoul@163.com>
2021-01-19 13:01:24 +0800

    Css

--- vueapp/src/components/page-view/index.vue
+++ vueapp/src/components/page-view/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page_view_html" @touchstart="start" @touchmove="move" @touchend="end">
+  <div class="page_view_html">
     <!-- header -->
     <div class="page_view_header" v-if="header" :style="{height:(height-10)+'px', lineHeight:(height-10)+'px', paddingTop:($store.state.statusBarHeight+5)+'px', paddingBottom:'5px', backgroundColor:bgColor, color:color}">
       <!-- Left -->
@@ -25,7 +25,7 @@
 
 <style scoped>
 .page_view_html{width: 100%; height: 100%;}
-.page_view_header{user-select: none; position: fixed; z-index: 999; width: 100%;}
+.page_view_header{user-select: none; position: fixed; z-index: 999; width: 100%; box-shadow: 0 1px 0 #F8F8F8;}
 .page_view_title{height: 100%; font-size: 16px; text-align: center;}
 .page_view_left{position: absolute; left: 16px; font-size: 14px;}
 .page_view_right{position: absolute; right: 16px;}
@@ -41,48 +41,15 @@ export default {
     header: {type: Boolean, default: true},
     color: {type: String, default: Env.statusBar.color},
     bgColor: {type: String, default: Env.statusBar.bgColor},
-    limit: {type: Number, default: 200},  //触发滑动方向
   },
   data(){
     return {
       height: Env.statusBar.height,
-      startPage: {x:0,y:0},
-      movePage: {x:0,y:0},
     }
   },
   mounted(){
   },
   methods:{
-
-    /* 开始 */
-    start(e){
-      let touch = e.touches?e.touches[0]:e;
-      this.startPage = {x:touch.clientX,y:touch.clientY};
-      this.movePage = {x:0,y:0};
-    },
-    /* 移动 */
-    move(e){
-      const touch = e.touches?e.touches[0]:e;
-      this.movePage = {
-        x: touch.clientX-this.startPage.x,
-        y: touch.clientY-this.startPage.y,
-      }
-    },
-    /* 结束 */
-    end(e){
-      // 滑动方向
-      const ratio = Math.abs(this.movePage.x/this.movePage.y) || 0;
-      if(ratio>1 && this.movePage.x>this.limit){
-        this.$emit('swipe','left');
-      }else if(ratio>1 && this.movePage.x<-this.limit){
-        this.$emit('swipe','right');
-      }else if(ratio<1 && this.movePage.y>this.limit){
-        this.$emit('swipe','down');
-      }else if(ratio<1 && this.movePage.y<-this.limit){
-        this.$emit('swipe','up');
-      }
-    },
-
   }
 }
 </script>
