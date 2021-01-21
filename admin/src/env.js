2dd9396fe1cbd773c650720e2199d6eb2c3bbcb9
webmis <klingsoul@163.com>
2021-01-21 16:57:37 +0800

    主题颜色

--- admin/src/env.js
+++ admin/src/env.js
@@ -22,6 +22,24 @@ export default {
   baseUrl: baseUrl, // 数据地址
   apiUrl: baseUrl+'admin/', // 数据接口地址
   copy: 'Copyright © WebMIS.vip 2020',  // 版权信息
+  /* 主题 */
+  themes: {
+    primary: '#6FB737', //主色
+    success: '#67C23A', //成功
+    warning: '#E6A23C', //警告
+    danger: '#F56C6C', //危险
+    info: '#909399', //信息
+    bgcolor: '#F2F4F8', //背景
+    text: '#303133', //主要文字
+    text1: '#606266', //常规文字
+    text2: '#909399', //次要文字
+    text3: '#C0C4CC', //占位文字
+    border: '#E2E4E8', //边框色
+    border1: '#DCDFE6', //一级边框
+    border2: '#E4E7ED', //二级边框
+    border3: '#EBEEF5', //三级边框
+    border4: '#F2F4F8', //四级边框
+  },
   /* 请求 */
   request:{
     headers: {
