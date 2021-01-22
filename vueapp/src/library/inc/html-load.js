40b6628a6955ca03433aa46b9adf14aaa9f88c16
webmis <klingsoul@163.com>
2021-01-22 17:43:24 +0800

    LoadHtml

--- vueapp/src/library/inc/html-load.js
+++ vueapp/src/library/inc/html-load.js
@@ -1,34 +1,34 @@
 /* 是否存在 */
-const _isInclude = (name)=>{
-  let js= /js$/i.test(name);
+const _isInclude = (name,type)=>{
+  let js= type=='js' || /js$/i.test(name);
   const es=document.getElementsByTagName(js?'script':'link');
   for(var i=0;i<es.length;i++) 
-  if(es[i][js?'src':'href'].indexOf(name)!=-1)return true;
+  if(es[i][js?'src':'href'].indexOf(name)!=-1) return true;
   return false;
 }
 
 /* 加载Css和JS */
-export default (files,reload)=>{
-  let file = files;
+export default (files,type,reload)=>{
   let ext = '';
   let isLoad = false;
   reload = reload || false;
-  for(let i=0; i<file.length;i++){
-    isLoad = reload?false:_isInclude(file[i]);
-    ext = file[i].split('.');
+  for(let i=0; i<files.length;i++){
+    isLoad = _isInclude(files[i],type);
+    if(isLoad && reload) isLoad=true;
+    ext = files[i].split('.');
     // JS
-    if(ext[ext.length-1]=='js'){
+    if(ext[ext.length-1]=='js' || type=='js'){
       if(!isLoad){
         let box = document.createElement('script');
-        box.setAttribute('src',file[i]);
+        box.setAttribute('src',files[i]);
         document.body.appendChild(box);
       }
     // CSS
-    }else if(ext[ext.length-1]=='css'){
+    }else if(ext[ext.length-1]=='css' || type=='css'){
       if(!isLoad){
         let box = document.createElement('link');
         box.setAttribute('rel','stylesheet');
-        box.setAttribute('href',file[i]);
+        box.setAttribute('href',files[i]);
         document.body.appendChild(box);
       }
     }
