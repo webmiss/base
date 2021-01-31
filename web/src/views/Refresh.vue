5235055aac429e5e8d0b38e56eeba31ff023de31
webmis <klingsoul@163.com>
2021-01-31 19:26:25 +0800

    Vue3 + TypeScript

--- web/src/views/Refresh.vue
+++ web/src/views/Refresh.vue
@@ -1,16 +1,14 @@
 <template>
 </template>
 <script>
-export default {
+import { defineComponent } from 'vue';
+export default defineComponent({
   beforeRouteEnter(to, from, next){
     next(vm=>{
       vm.$router.replace(from.path);
     });
   },
   mounted(){
-    // 动作菜单-配置
-    this.$store.state.action.url = '';
-    this.$store.state.action.menus = '';
   },
-}
+});
 </script>
