a287c5d70c18dc98ddfe4ff8c28289cc426c4a57
webmis <klingsoul@163.com>
2021-02-02 07:25:42 +0800

    清理文件

--- phalcon/public/themes/library/inc/html-img.js
+++
@@ -1,8 +0,0 @@
-/* 获取图片地址 */
-export default (html)=>{
-  const reg = /<img.+?src=('|")?([^'"]+)('|")?(?:\s+|>)/gim;
-  let imgs = [];
-  let tem = null;
-  while (tem=reg.exec(html)) imgs.push(tem[2]);
-  return imgs;
-}
