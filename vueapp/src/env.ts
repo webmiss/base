05ca7c4bba942c4b8ed3ee16a7257591465cabe1
webmis <klingsoul@163.com>
2021-02-04 12:00:27 +0800

    bug

--- vueapp/src/env.ts
+++ vueapp/src/env.ts
@@ -1,5 +1,10 @@
-const baseUrl = 'https://demo-php.webmis.vip/';
-const socket = 'wss://demo-php.webmis.vip/wss';
+/* 开发 */
+const baseUrl = 'http://localhost:9010/';
+const socket = 'ws://localhost:9011/';
+
+/* 正式 */
+// const baseUrl = 'https://demo-php.webmis.vip/';
+// const socket = 'wss://demo-php.webmis.vip/wss';
 
 export default {
   title: 'WebMIS VueAPP', // 应用名称
