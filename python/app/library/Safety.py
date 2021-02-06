7ce470189fc265ae026b60151c206ba0311181bf
webmis <klingsoul@163.com>
2021-02-06 12:54:12 +0800

    bug

--- python/app/library/Safety.py
+++ python/app/library/Safety.py
@@ -24,7 +24,7 @@ class Safety:
   def encode(param={}):
     try :
       token = jwt.encode(param, Env.key, algorithm='HS256')
-      return bytes.decode(token)
+      return bytes.decode(token) if type(token)==bytes else token
     except Exception as e :
       return None
 
