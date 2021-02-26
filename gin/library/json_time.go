65863d72efb67fe232a0dad52594f7221becece0
webmis <klingsoul@163.com>
2021-02-26 15:32:27 +0800

    Gin

---
+++ gin/library/json_time.go
@@ -0,0 +1,13 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+type JsonTime time.Time
+
+func (this JsonTime) MarshalJSON() ([]byte, error) {
+	var stamp = fmt.Sprintf("\"%s\"", time.Time(this).Format("2006-01-02 15:04:05"))
+	return []byte(stamp), nil
+}
