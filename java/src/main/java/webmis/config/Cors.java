59d13b9425ccb66d1bae0282e1809f8d31cc376d
webmis <klingsoul@163.com>
2021-03-08 20:59:57 +0800

    允许跨域请求

---
+++ java/src/main/java/webmis/config/Cors.java
@@ -0,0 +1,28 @@
+package webmis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/* 允许跨域请求 */
+@Configuration
+public class Cors {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.addAllowedOrigin("*"); // 允许域名
+        cors.addAllowedHeader("*"); // 允许任何头
+        cors.addAllowedMethod("*"); // 允许请求方式
+        return cors;
+    }
+ 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // source.registerCorsConfiguration("/admin/**", buildConfig());
+        // source.registerCorsConfiguration("/api/**", buildConfig());
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
