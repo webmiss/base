abdab389ba32077aa11ff4583e0f4d7d4d8f1f5f
webmis <klingsoul@163.com>
2021-04-06 17:39:33 +0800

    Socket

--- phalcon/config/Env.php
+++ phalcon/config/Env.php
@@ -4,8 +4,8 @@ namespace Config;
 /* 公共配置 */
 class Env {
 
-  // static $base_url = 'https://demo-php.webmis.vip/';     //资源
-  static $base_url = 'http://localhost/phalcon/public/';    //资源
+  static $base_url = 'https://demo-php.webmis.vip/';        //资源
+  // static $base_url = 'http://localhost/phalcon/public/';    //资源
   static $root_dir = '';                                    //根目录
   static $key = 'e4b99adec618e653400966be536c45f8';         //KEY
   /* Token */
