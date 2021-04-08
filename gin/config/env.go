6fae1c9455be205816d46aa5881ea898cf98e88b
webmis <klingsoul@163.com>
2021-04-08 10:18:18 +0800

    配置

--- gin/config/env.go
+++ gin/config/env.go
@@ -18,18 +18,18 @@ type EnvType struct {
 /* 公共配置 */
 func Env() *EnvType {
 	cfg := &EnvType{}
-	cfg.Mode = "debug"                           //模式: debug, release, test
+	cfg.Mode = "release"                         //模式: debug, release, test
 	cfg.Host = "127.0.0.1"                       //主机
 	cfg.Port = "9030"                            //端口
 	cfg.Key = "e4b99adec618e653400966be536c45f8" //Key
-	// cfg.BaseURL = "https://demo-go.webmis.vip/"  //资源
-	cfg.BaseURL = "http://localhost/gin/public/" //资源
-	cfg.RootDir = "public/"                      //根目录
-	cfg.AdminTokenPrefix = "Admin"               //前缀
-	cfg.AdminTokenTime = 2 * 3600                //有效时长(2小时)
-	cfg.AdminTokenAuto = true                    //自动续期
-	cfg.ApiTokenPrefix = "Api"                   //前缀
-	cfg.ApiTokenTime = 7 * 24 * 3600             //有效时长(7天)
-	cfg.ApiTokenAuto = true                      //自动续期
+	cfg.BaseURL = "https://demo-go.webmis.vip/"  //资源
+	// cfg.BaseURL = "http://localhost/gin/public/" //资源
+	cfg.RootDir = "public/"          //根目录
+	cfg.AdminTokenPrefix = "Admin"   //前缀
+	cfg.AdminTokenTime = 2 * 3600    //有效时长(2小时)
+	cfg.AdminTokenAuto = true        //自动续期
+	cfg.ApiTokenPrefix = "Api"       //前缀
+	cfg.ApiTokenTime = 7 * 24 * 3600 //有效时长(7天)
+	cfg.ApiTokenAuto = true          //自动续期
 	return cfg
 }
