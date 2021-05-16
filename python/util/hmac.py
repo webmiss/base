3855d82f09fa3cacf4da0e6aa0ca98776054f9e1
webmis <klingsoul@163.com>
2021-05-16 10:56:25 +0800

    Bug

--- python/util/hmac.py
+++ python/util/hmac.py
@@ -6,7 +6,7 @@ class Hmac:
 
   # Md5
   def Md5(data: str):
-    return hashlib.md5(str.encode(encoding='utf-8')).hexdigest()
+    return hashlib.md5(data.encode(encoding='utf-8')).hexdigest()
 
   # Sha256
   def Sha256(data: str, key: str):
