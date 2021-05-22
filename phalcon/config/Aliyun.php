76c8cfe2da2d16131ae04533f01ff8a1f7dad1c1
webmis <klingsoul@163.com>
2021-05-22 18:08:08 +0800

    阿里云配置

---
+++ phalcon/config/Aliyun.php
@@ -0,0 +1,17 @@
+<?php
+namespace Config;
+
+/* 阿里云配置 */
+class Tencent {
+
+  /* RAM访问控制 */
+  static function RAM(): array {
+    return [
+      'UserId'=> 247834896167087138,
+      'UserName'=> 'AliyunOSSTokenGeneratorUser@mrcui.onaliyun.com',
+      'AccessKeyId'=> 'LTAI5tBpaEBk8Texmp2mjhQj',
+      'AccessKeySecret'=> 'DpocHAjH9z1FhO3lqJh8KSrrO3AcqO',
+    ];
+  }
+
+}
