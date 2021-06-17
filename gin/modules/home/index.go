eb4bac24899425e089cf1d427eab2509a3847c11
webmis <klingsoul@163.com>
2021-06-17 11:23:52 +0800

    OSS回调

--- gin/modules/home/index.go
+++ gin/modules/home/index.go
@@ -30,7 +30,7 @@ func (r Index) UpFileCallback(c *gin.Context) {
 	authorizationBase64 := c.Request.Header.Get("authorization")
 	bodyContent, _ := ioutil.ReadAll(c.Request.Body)
 	callbackBody := string(bodyContent)
-	fmt.Printf("RawPath={%s}, Query()={%s}, CallbackBody={%s}\n", c.Request.URL.RawPath, c.Request.URL.Query(), callbackBody)
+	fmt.Printf("RawPath={%s}, Query()={%s}, CallbackBody={%s}\n", c.Request.URL.RawPath, c.Request.RequestURI, callbackBody)
 	// 验证
 	publicKeyUrlBase64 = "aHR0cHM6Ly9nb3NzcHVibGljLmFsaWNkbi5jb20vY2FsbGJhY2tfcHViX2tleV92MS5wZW0="
 	authorizationBase64 = "lRwZVdeqeee91Ma6k+Wafk0dRw8HfMJOJLQEom8eLW4CZbk89I+8dQfhgQLbbYbZAlnBSMobnt3BmT8KLfKonA=="
