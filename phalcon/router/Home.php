aa184bda5d5eacdf0c09290a578f8d074758f9d0
webmis <klingsoul@163.com>
2022-03-21 14:08:37 +0800

    Test

--- phalcon/router/Home.php
+++ phalcon/router/Home.php
@@ -5,6 +5,7 @@ class Home {
   static function Init($app){
     // 首页
     $app->get('/', 'App\Home\Index::Index');
+    $app->get('/test', 'App\Home\Index::Test');
     $app->get('/vcode', 'App\Home\Index::Vcode');
     $app->get('/index/qrcode/{name}', 'App\Home\Index::Qrcode');
     $app->post('/ossCallback', 'App\Home\Index::OssCallback');
